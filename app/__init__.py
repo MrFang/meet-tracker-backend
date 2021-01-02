067877a11d8a8373aa03a7c5e7c5d2f6d53d5426
MrFang <bat312@yandex.ru>
2021-01-02 18:57:26 +0300

    Add db

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
+from . import db
+
+import os
 
 
-def create_app(test_config=None):
+def create_app():
     app = Flask(
         __name__,
+        instance_relative_config=True,
         static_folder='../dist',
-        static_url_path='',
-        template_folder='../dist'
+        template_folder='../dist',
+        static_url_path=''
     )
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    db.init_app(app)
     
     @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
     def catch_all(path):
-        return render_template("index.html")
+        return render_template('index.html')
 
     return app
