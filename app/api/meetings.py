319cad19f73838bb9f4a291cc32971db2809c434
MrFang <bat312@yandex.ru>
2021-01-11 20:42:49 +0300

    Add API for meeting update

--- app/api/meetings.py
+++ app/api/meetings.py
@@ -75,7 +75,6 @@ def delete(request_data):
     id = request_data.get('id')
     db = get_db()
     error = None
-    meeting= None
 
     if id is None:
         error = 'Meeting ID is required'
@@ -101,3 +100,52 @@ def delete(request_data):
             'error': error,
             'data': None
         }
+
+
+def update(request_data):
+    id = request_data.get('id')
+    title = request_data.get('title')
+    datetime_string = request_data.get('datetime')
+    db = get_db()
+    error = None
+
+    if id is None:
+        error = 'Meeting ID is required'
+    elif title is None:
+        error = 'Title is required'
+    elif datetime_string is None:
+        error = 'Datetime is required'
+    else:
+        try:
+            datetime.datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M')
+        except ValueError:
+            error = 'Datetime string is invalid'
+    
+    if error is None:
+        meeting = db.execute('SELECT * FROM meeting WHERE id = ?', (id,)).fetchone()
+
+        if meeting is None:
+            error = 'There is no meeting with such ID'
+    
+    if error is None:
+        db.execute(
+            'UPDATE meeting SET ' +
+            'title = ?, ' +
+            'datetime = ? ' +
+            'WHERE id = ?',
+            (title, datetime_string, id)
+        )
+        db.commit()
+
+        return {
+            'success': True,
+            'error': error,
+            'data': None
+        }
+    else: 
+        return {
+            'success': False,
+            'error': error,
+            'data': None
+        }
+
